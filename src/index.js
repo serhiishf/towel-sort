56ebcc99e77b18e8e7a17983621030b69ebf9921
Сергей Ш <unison447@gmail.com>
2022-02-06 17:56:12 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
+
+
 module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+  if(matrix){
+    matrix.forEach((el, index) => {
+      while(el.length !== 0){
+            if(index % 2 === 0){
+        result.push(el.shift());
+      }
+      else{
+        result.push(el.pop());
+      }
+      }
+  
+    })
+  }
+  
+  return result;
 }
